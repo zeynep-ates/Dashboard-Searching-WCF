ef6961948654f26ec3338e8d7757a4c8ae9e5c36
Zeynep Ateş <zeynep.ates@ozu.edu.tr>
2024-08-17 16:08:32 +0300

    Update Service1.cs

--- Service1.cs
+++ Service1.cs
@@ -7,7 +7,6 @@ using System.Text;
 
 namespace DashboardWCF2Lib
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
     public class Service1 : IService1
     {
         SearchingClass searchingClass = new SearchingClass();
