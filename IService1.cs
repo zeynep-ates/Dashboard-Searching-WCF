ba7f97b97f89b83795b4b8481684b52f5c84b3e4
Zeynep Ateş <zeynep.ates@ozu.edu.tr>
2024-08-17 16:09:04 +0300

    Update IService1.cs

--- IService1.cs
+++ IService1.cs
@@ -7,7 +7,6 @@ using System.Text;
 
 namespace DashboardWCF2Lib
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
     public interface IService1
     {
@@ -15,12 +14,8 @@ namespace DashboardWCF2Lib
         List<SearchResult> SearchID(string telemetryID);
         [OperationContract]
         List<SearchResult> SearchName(string telemetryName);
-
-        // TODO: Add your service operations here
     }
 
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "DashboardWCF2Lib.ContractType".
     [DataContract]
     public class CompositeType
     {
